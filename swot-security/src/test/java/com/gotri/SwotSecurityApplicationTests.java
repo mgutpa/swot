26a0dfe45e2940c57ebb31f3e063b530e0bfd869
mantu.gupta <mantu.gupta@STLERI.COM>
2020-11-24 00:29:18 +0530

    commiting security sample code

---
+++ swot-security/src/test/java/com/gotri/SwotSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwotSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
